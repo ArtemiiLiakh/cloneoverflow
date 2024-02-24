6d8f96e535f32c9b525e43d2edf203e47e536cba
Denys <mamchyk.den@gmail.com>
2024-02-24 17:39:23 +0200

    repository #2 Create repository for answers
    - created DbAnswer
    - created repository for answer
    
    closes #2

---
+++ backend/src/utils/types/DbAnswer.ts
@@ -0,0 +1,14 @@
+import {Question, UserProfile} from "@prisma/client";
+
+export class DbAnswer{
+    id: string;
+    userId: string;
+    userProfile: UserProfile;
+    questionId: string;
+    question: Question;
+    text: string;
+    rate: number;
+    isSolution: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+};
